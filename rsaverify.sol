48d7ca5d1a1ab0d3b1ef8e137d0d091c9a7a0086
Alex Beregszaszi <alex@rtfs.hu>
2016-04-06 13:20:23 +0100

    Check return value

--- rsaverify.sol
+++ rsaverify.sol
@@ -17,13 +17,18 @@ library RSAVerify {
         bytes memory req = new bytes(len - 4);
         bytes memory res = new bytes(32);
 
+        uint status;
+
         assembly {
             let alen := len
             calldatacopy(req, 4, alen)
-            call(sub(gas, 150), 5, 0, req, alen, res, 32)
-            // FIXME: check for return value (failed call)
-            pop
-            return (res, 32)
+            call(sub(gas, 150), 5, 0, req, alen, add(res, 32), 32)
+            =: status
         }
+
+        if (status != 1)
+          return false;
+
+        return res[31] == 1;
      }
 }
